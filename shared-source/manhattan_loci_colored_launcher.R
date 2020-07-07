4e054f11a92794686272d732084b0a10f426a47c
palmercd <cameron.palmer@nih.gov>
2020-07-07 14:37:30 -0400

    Intermittent plotting boundary error appears to be due to aberrantly small p-values; this is evidently why I included the p-truncation option, a million years ago

--- shared-source/manhattan_loci_colored_launcher.R
+++ shared-source/manhattan_loci_colored_launcher.R
@@ -4,7 +4,8 @@ trait.name <- cargs[2]
 output.filestem <- cargs[3]
 source.dir <- cargs[4]
 gws.threshold <- 5e-8
+truncate.p <- 1e-50
 
 source(paste(source.dir, "/manhattan_loci_colored.R", sep=""))
 
-manhattan.coded.hits(filename.pvals, trait.name, output.filestem=output.filestem, gws.threshold=gws.threshold)
+manhattan.coded.hits(filename.pvals, trait.name, output.filestem=output.filestem, gws.threshold=gws.threshold, truncate.p=truncate.p)
