e765ac0525ce84df6cdab5969b6a343cc1ac65f9
palmercd <cameron.palmer@nih.gov>
2020-08-07 11:53:39 -0400

    Added test support for categorical variable analysis with saige. Preprocessor partitions categorical variables into multiple binary trait analyses with the same reference level and provides those prefixes to Make. Prefixes are {standard-prefix}/comparison{NUMBER}. Added handling of category levels tracker for all saige traits to R model matrix generation (not heavily tested :( ). Added placeholder support for post-analysis merging in metal pipeline, pending the completion of the first test runs. Added configuration for cig_stat categorical variable. Updated some config tests. Updated gitignore for some DAG generation testing

--- shared-makefiles/Makefile.metal
+++ shared-makefiles/Makefile.metal
@@ -3,8 +3,16 @@
 
 include $(PROJECT_BASE_DIR)/Makefile.config
 
-ALL_TARGETS := $(sort $(dir $(shell find $(RESULT_OUTPUT_DIR) -name "*[ame].tsv.gz" -print)))
+ALL_FIND_TARGETS := $(shell find $(RESULT_OUTPUT_DIR) -name "*[ame].tsv.gz" -print)
+ALL_TARGETS := $(sort $(dir $(shell echo $(ALL_FIND_TARGETS) | grep -vE "*/comparison[:digit:]+/*")))
 ALL_TARGETS := $(foreach target,$(ALL_TARGETS),$(target)$(word 1,$(subst /, ,$(subst $(RESULT_OUTPUT_DIR),,$(target)))).$(word 2,$(subst /, ,$(subst $(RESULT_OUTPUT_DIR),,$(target)))).$(word 3,$(subst /, ,$(subst $(RESULT_OUTPUT_DIR),,$(target)))).tsv)
+
+## handle categorical analysis targets separately, as they're split into separate binary outcomes versus a reference and must be preprocessed
+#CATEGORICAL_TARGETS := $(sort $(dir $(shell echo $(ALL_FIND_TARGETS) | grep -E "*/comparison[:digit:]+/*")))
+#CATEGORICAL_TARGETS := $(foreach target,$(CATEGORICAL_TARGETS),$(word 1,$(subst /comparison, ,$(target))))
+#CATEGORICAL_TARGETS := $(foreach target,$(CATEGORICAL_TARGETS),$(target)$(word 1,$(subst /, ,$(subst $(RESULT_OUTPUT_DIR),,$(target)))).$(word 2,$(subst /, ,$(subst $(RESULT_OUTPUT_DIR),,$(target)))).$(word 3,$(subst /, ,$(subst $(RESULT_OUTPUT_DIR),,$(target)))).tsv)
+#$(info $(CATEGORICAL_TARGETS))
+
 METAL := $(METAL_EXECUTABLE)
 
 $(if $(EXCLUDE_SAIGE),$(eval ALL_TARGETS:=$(filter-out %.SAIGE.tsv %.saige.tsv,$(ALL_TARGETS))),)
@@ -18,7 +26,7 @@ $(info $(ALL_TARGETS))
 .SECONDEXPANSION:
 .PHONY: all
 
-all: $(addsuffix .gz,$(ALL_TARGETS))
+all: $(addsuffix .gz,$(ALL_TARGETS)) $(if $(EXCLUDE_SAIGE),,$(addsuffix .gz,$(CATEGORICAL_TARGETS)))
 
 ## patterns:
 ##    output: results/{PHENOTYPE}/{ANCESTRY}/{METHOD}/{PHENOTYPE}.{METHOD}.tsv.gz
@@ -31,7 +39,7 @@ all: $(addsuffix .gz,$(ALL_TARGETS))
 ##    output: results/{PHENOTYPE}/{ANCESTRY}/{METHOD}/{PHENOTYPE}.{METHOD}.tsv
 ##    input:  results/{PHENOTYPE}/{ANCESTRY}/{METHOD}/{PHENOTYPE}.{METHOD}1.raw.tsv.success
 ## Notes: reformat output to match standard atlas output format, with added heterogeneity p-value
-$(ALL_TARGETS): $$(subst .tsv,1.raw.tsv.success,$$@)
+$(ALL_TARGETS) $(CATEGORICAL_TARGETS): $$(subst .tsv,1.raw.tsv.success,$$@)
 	awk 'NR > 1 {print $$1"\t"$$2"\t"$$3"\t"$$4"\t"$$8"\t"$$9"\t"$$10"\t"$$16"\t"$$15}' $(subst .success,,$<) | sed 's/^chr// ; s/:/\t/g' | awk 'NR == 1 {print "CHR\tPOS\tSNP\tTested_Allele\tOther_Allele\tFreq_Tested_Allele_in_TOPMed\tBETA\tSE\tP\tN\tPHet"} ; {print $$1"\t"$$2"\tchr"$$1":"$$2":"$$3":"$$4"\t"toupper($$5)"\t"toupper($$6)"\t"$$7"\t"$$8"\t"$$9"\t"$$10"\t"$$11"\t"$$12}' > $@
 
 ## patterns:
@@ -45,5 +53,12 @@ $(ALL_TARGETS): $$(subst .tsv,1.raw.tsv.success,$$@)
 ##    output: results/{PHENOTYPE}/{ANCESTRY}/{METHOD}/{PHENOTYPE}.{METHOD}1.par
 ##    input:  results/{PHENOTYPE}/{ANCESTRY}/{METHOD}/{PHENOTYPE}.{CHIP}.{METHOD}.tsv
 ## Notes: barebones configuration for metal. assumes effect estimates in log(OR) space for the moment.
-%.par: $$(shell find $$(dir $$@) -name "*[eam].tsv.gz" -print | sed 's/.gz$$$$//')
+$(subst .tsv,1.par,$(ALL_TARGETS)): $$(shell find $$(dir $$@) -name "*[eam].tsv.gz" -print | sed 's/.gz$$$$//')
+$(subst .tsv,1.par,$(CATEGORICAL_TARGETS)): $$(foreach target,$$(shell find $$(dir $$@) -name "*saige.tsv.gz" -print | sed 's/.gz$$$$//'),$$(firstword $$(subst /processed, ,$$(target)))/$$(notdir $$(target)))
+
+%.par:
 	echo -e "MARKERLABEL SNP\nALLELELABELS Tested_Allele Other_Allele\nEFFECTLABEL BETA\nSTDERRLABEL SE\nFREQLABEL Freq_Tested_Allele_in_TOPMed\nCUSTOMVARIABLE TotalSampleSize\nLABEL TotalSampleSize as N\nSCHEME STDERR\nGENOMICCONTROL ON\nAVERAGEFREQ ON\nMINMAXFREQ ON\n$(patsubst %,PROCESSFILE %\n,$^)OUTFILE $(subst 1.par,,$@) .raw.tsv\nANALYZE HETEROGENEITY\nQUIT" > $@
+
+%.saige.tsv.gz: $$(find $$(dir $$@)/comparison* -name "$$(notdir $$@)" -print)
+	./combine_categorical_runs.out $^ $@
+
--- shared-source/construct_output_filenames.bash
+++ shared-source/construct_output_filenames.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$#" -ne 17 ]; then
-    echo "the command 'construct_output_filenames.bash' requires sixteen command line arguments: the name of the config file, the software (saige, bolt, fastgwa) requested, the minimum sample count for the software, the directory prefix for all bgen files, the pipeline results toplevel directory, the phenotype file currently in use, the column extractor program, the phenotype file used tracker file suffix, the frequency mode tracker file suffix, the id mode tracker file suffix, the phenotype model tracker file suffix, the covariate model tracker file suffix, the phenotype transformation tracker file suffix, the sex-specific analysis selection tracker file suffix, the finalized analysis tracker file suffix, binary indicator of whether make -B is in effect, and a binary indicator of whether make -n is in effect"
+if [ "$#" -ne 18 ]; then
+    echo "the command 'construct_output_filenames.bash' requires sixteen command line arguments: the name of the config file, the software (saige, bolt, fastgwa) requested, the minimum sample count for the software, the directory prefix for all bgen files, the pipeline results toplevel directory, the phenotype file currently in use, the column extractor program, the phenotype file used tracker file suffix, the frequency mode tracker file suffix, the id mode tracker file suffix, the phenotype model tracker file suffix, the covariate model tracker file suffix, the category level tracker file suffix, the phenotype transformation tracker file suffix, the sex-specific analysis selection tracker file suffix, the finalized analysis tracker file suffix, binary indicator of whether make -B is in effect, and a binary indicator of whether make -n is in effect"
 else
     CONFIG_FILE="$1"
     REQUESTED_SOFTWARE="$2"
@@ -14,11 +14,12 @@ else
     ID_MODE_TRACKER_SUFFIX="${10}"
     PHENOTYPE_SELECTED_TRACKER_SUFFIX="${11}"
     COVARIATES_SELECTED_TRACKER_SUFFIX="${12}"
-    TRANSFORMATION_TRACKER_SUFFIX="${13}"
-    SEX_SPECIFIC_TRACKER_SUFFIX="${14}"
-    FINALIZED_ANALYSIS_TRACKER_SUFFIX="${15}"
-    FORCE_RUN="${16}"
-    PRETEND_RUN="${17}"
+    CATEGORY_TRACKER_SUFFIX="${13}"
+    TRANSFORMATION_TRACKER_SUFFIX="${14}"
+    SEX_SPECIFIC_TRACKER_SUFFIX="${15}"
+    FINALIZED_ANALYSIS_TRACKER_SUFFIX="${16}"
+    FORCE_RUN="${17}"
+    PRETEND_RUN="${18}"
 
     ## python yaml helper function
     yaml() {
@@ -35,6 +36,21 @@ else
     REQUESTED_CHIPS=$(yaml "$CONFIG_FILE" "chips")
     REQUESTED_ANCESTRIES=$(yaml "$CONFIG_FILE" "ancestries")
     REQUESTED_ALGORITHM=$(yaml "$CONFIG_FILE" "algorithm")
+
+    ## only compute this either the first time, or when the analysis as a whole is being reevaluated
+    PHENOTYPE=$(yaml "$CONFIG_FILE" "phenotype")
+    FACTORIZATION="NA"
+    if [[ "${REQUESTED_ALGORITHM^^}" == *"SAIGE"* ]] ; then
+    FACTORIZATION=`awk -F"\t" -v trait=$PHENOTYPE 'BEGIN {COL=0}
+    		       NR == 1 { for ( i = 1 ; i <= NF ; i++ ) { if ( $i == trait ) {COL=i} } }
+		       NR > 1 { print $COL }' "$PHENOTYPE_FILENAME" | sort | uniq -c | awk '$2 != "NA"' | sort -k 1,1gr |
+		          awk 'BEGIN {nsmall=0}
+			       NR == 1 {ref=$2}
+			       NR > 1 && $1 >= 100 {print ref"\t"$2}
+			       NR > 1 && $1 < 100  {alt[$2] ; nsmall++}
+			       END { if (nsmall > 0) { printf("%s\t", ref) ; for ( i in alt ) {printf("%s+", i)}}}' | awk '{print $0}' | sed 's/+$//'`
+    fi
+    
     for chip in $REQUESTED_CHIPS;
     do
 	for ancestry in $REQUESTED_ANCESTRIES;
@@ -113,7 +129,6 @@ else
 		    fi
 
 		    ## run phenotype selection tracking/version difference testing
-		    PHENOTYPE=$(yaml "$CONFIG_FILE" "phenotype")
 		    if [[ -f "$PHENOTYPE_SELECTED_TRACKER" ]] ; then
 			USED_PHENOTYPE=`cat $PHENOTYPE_SELECTED_TRACKER`
 			if [[ "$PHENOTYPE" != "$USED_PHENOTYPE" ]] ; then
@@ -137,8 +152,9 @@ else
 		    else
 			UPDATE_BUNDLE="1"
 		    fi
-		    
+
 		    ## run frequency reporting mode tracking/version difference testing
+		    FREQUENCY_UPDATED="0"
 		    if [[ "$PRETEND_RUN" -eq "0" ]] ; then
 			FREQUENCY_MODE="reference"
 			if [[ ! -z $(yaml_check_exists "$CONFIG_FILE" "frequency_mode") ]] ; then
@@ -148,18 +164,22 @@ else
 			    EXISTING_FREQUENCY_MODE=`cat $FREQUENCY_MODE_TRACKER`
 			    if [[ "$EXISTING_FREQUENCY_MODE" != "$FREQUENCY_MODE" ]] ; then
 				echo "$FREQUENCY_MODE" > "$FREQUENCY_MODE_TRACKER"
+				FREQUENCY_UPDATED="1"
 			    fi
 			else
 			    echo "$FREQUENCY_MODE" > "$FREQUENCY_MODE_TRACKER"
+			    FREQUENCY_UPDATED="1"
 			    rm -f "$FINALIZED_ANALYSIS_TRACKER"
 			fi
 			## allow manual override
 			if [[ "$FORCE_RUN" -gt "0" ]] ; then
+			    FREQUENCY_UPDATED="1"
 			    echo "$FREQUENCY_MODE" > "$FREQUENCY_MODE_TRACKER"
 			fi
 		    fi
 
 		    ## run ID reporting mode tracking/version difference testing
+		    ID_UPDATED="0"
 		    if [[ "$PRETEND_RUN" -eq "0" ]] ; then
 			ID_MODE="chrpos"
 			if [[ ! -z $(yaml_check_exists "$CONFIG_FILE" "id_mode") ]] ; then
@@ -168,14 +188,17 @@ else
 			if [[ -f "$ID_MODE_TRACKER" ]] ; then
 			    EXISTING_ID_MODE=`cat $ID_MODE_TRACKER`
 			    if [[ "$EXISTING_ID_MODE" != "$ID_MODE" ]] ; then
+				ID_UPDATED="1"
 				echo "$ID_MODE" > "$ID_MODE_TRACKER"
 			    fi
 			else
+			    ID_UPDATED="1"
 			    echo "$ID_MODE" > "$ID_MODE_TRACKER"
 			    rm -f "$FINALIZED_ANALYSIS_TRACKER"
 			fi
 			## allow manual override
 			if [[ "$FORCE_RUN" -gt "0" ]] ; then
+			    ID_UPDATED="1"
 			    echo "$ID_MODE" > "$ID_MODE_TRACKER"
 			fi
 		    fi
@@ -230,7 +253,74 @@ else
 		    ## and none of the configuration tracking files have changed, then do not report this
 		    ## analysis as a tracking candidate to the workflow unless make -B has been invoked.
 		    if [[ ! -f "$FINALIZED_ANALYSIS_TRACKER" ]] ; then
-			echo "$RESULT$REPORT_SUFFIX"
+			SUPPRESS_CENTRAL_DIRECTORY="0"
+			## run category tracking. this is a complicated hack to make the SAIGE binary pipeline work with categorical variables
+			## skip when outcome is continuous
+			if [[ "$CATEGORY_TRACKER_SUFFIX" != "NA" ]] ; then
+			    N_COMPARISONS=`echo "$FACTORIZATION" | wc -l`
+			    if [[ "$N_COMPARISONS" -gt "1" ]] ; then
+				## there is more than one comparison to be done; this requires splitting the analysis into multiple directories :(
+				for i in `seq 1 $N_COMPARISONS` ;
+				do
+				    ## build the new location information
+				    RESULTDIR="$ANALYSIS_PREFIX/${ancestry}/${REQUESTED_SOFTWARE^^}/comparison$i"
+				    RESULT="$RESULTDIR/$ANALYSIS_PREFIX.$chip.$REQUESTED_SOFTWARE"
+				    PHENOTYPE_USED_TRACKER_CMP="$RESULTS_PREFIX/$RESULT$PHENOTYPE_USED_TRACKER_SUFFIX"
+				    FREQUENCY_MODE_TRACKER_CMP="$RESULTS_PREFIX/$RESULT$FREQUENCY_MODE_TRACKER_SUFFIX"
+				    ID_MODE_TRACKER_CMP="$RESULTS_PREFIX/$RESULT$ID_MODE_TRACKER_SUFFIX"
+				    PHENOTYPE_SELECTED_TRACKER_CMP="$RESULTS_PREFIX/$RESULT$PHENOTYPE_SELECTED_TRACKER_SUFFIX"
+				    COVARIATES_SELECTED_TRACKER_CMP="$RESULTS_PREFIX/$RESULT$COVARIATES_SELECTED_TRACKER_SUFFIX"
+				    TRANSFORMATION_TRACKER_CMP="$RESULTS_PREFIX/$RESULT$TRANSFORMATION_TRACKER_SUFFIX"
+				    SEX_SPECIFIC_TRACKER_CMP="$RESULTS_PREFIX/$RESULT$SEX_SPECIFIC_TRACKER_SUFFIX"
+				    FINALIZED_ANALYSIS_TRACKER_CMP="$RESULTS_PREFIX/$RESULT$FINALIZED_ANALYSIS_TRACKER_SUFFIX"
+				    CATEGORY_TRACKER_CMP="$RESULTS_PREFIX/$RESULT$CATEGORY_TRACKER_SUFFIX"
+				    ## make the directory if needed
+				    mkdir -p "$RESULTS_PREFIX/$RESULTDIR"
+				    ## copy trackers over
+				    if [[ "$UPDATE_BUNDLE" -gt "0" ]] ; then
+					cp "$PHENOTYPE_USED_TRACKER" "$PHENOTYPE_USED_TRACKER_CMP"
+					cp "$PHENOTYPE_USED_TRACKER.history" "$PHENOTYPE_USED_TRACKER_CMP.history"
+					if [[ "$FREQUENCY_UPDATED" -gt "0" ]] ; then
+					    cp "$FREQUENCY_MODE_TRACKER" "$FREQUENCY_MODE_TRACKER_CMP"
+					fi
+					if [[ "$ID_UPDATED" -gt "0" ]] ; then
+					    cp "$ID_MODE_TRACKER" "$ID_MODE_TRACKER_CMP"
+					fi
+					cp "$PHENOTYPE_SELECTED_TRACKER" "$PHENOTYPE_SELECTED_TRACKER_CMP"
+					cp "$COVARIATES_SELECTED_TRACKER" "$COVARIATES_SELECTED_TRACKER_CMP"
+					cp "$TRANSFORMATION_TRACKER" "$TRANSFORMATION_TRACKER_CMP"
+					cp "$SEX_SPECIFIC_TRACKER" "$SEX_SPECIFIC_TRACKER_CMP"
+					NEW_CATEGORIES=`echo "$FACTORIZATION" | awk -v target=$i 'NR == target' | sed 's/\t/\n/g ; s/+/\n/g'`
+					if [[ ! -f "$CATEGORY_TRACKER_CMP" ]] ; then
+					    echo "$NEW_CATEGORIES" > "$CATEGORY_TRACKER_CMP"
+					else
+					    OLD_CATEGORIES=`cat $CATEGORY_TRACKER`
+					    if [[ "$NEW_CATEGORIES" != "$OLD_CATEGORIES" ]] ; then
+						echo "$NEW_CATEGORIES" > "$CATEGORY_TRACKER_CMP"
+					    fi
+					fi
+				    fi
+				    ## emit the directory as an analysis target
+				    echo "$RESULT$REPORT_SUFFIX"
+				done
+				SUPPRESS_CENTRAL_DIRECTORY="1"
+			    else
+				## there is only one comparison. this is very good. still needs to be emitted
+				CATEGORY_TRACKER="$RESULTS_PREFIX/$RESULT$CATEGORY_TRACKER_SUFFIX"
+				NEW_CATEGORIES=`echo "$FACTORIZATION" | sed 's/\t/\n/g ; s/+/\n/g'`
+				if [[ ! -f "$CATEGORY_TRACKER" ]] ; then
+				    echo "$NEW_CATEGORIES" > "$CATEGORY_TRACKER"
+				else
+				    OLD_CATEGORIES=`cat $CATEGORY_TRACKER`
+				    if [[ "$NEW_CATEGORIES" != "$OLD_CATEGORIES" ]] ; then
+					echo "$NEW_CATEGORIES" > "$CATEGORY_TRACKER"
+				    fi
+				fi
+			    fi
+			fi
+			if [[ "$SUPPRESS_CENTRAL_DIRECTORY" -eq "0" ]] ; then
+			    echo "$RESULT$REPORT_SUFFIX"
+			fi
 		    fi
 		fi
 	    fi
