9a478c7ea98d6aa66835441c42d9c00b5e0a437e
palmercd <cameron.palmer@nih.gov>
2020-08-23 09:03:28 -0400

    Added support for known signals plotting. Modified behavior for known signals existence in input files to handle realities of dup variants in post-rsid-conversion analysis files

--- shared-source/manhattan_loci_colored.R
+++ shared-source/manhattan_loci_colored.R
@@ -173,17 +173,19 @@ manhattan.coded.hits <- function(filename.pvals,
                 snp <- prev.hits[i,1]
                 snp.label <- prev.hits[i,2]
                 print(paste("annotating snp ", snp, sep=""))
-                stopifnot(length(raw.data[,pos.col.header][raw.data[,rsid.col.header] == snp]) == 1)
-                locus.pos.center <- raw.data[,pos.col.header][raw.data[,rsid.col.header] == snp]
-                all.colors[abs(raw.data[,pos.col.header] - locus.pos.center) < locus.width] <- prev.loci.color
+                ##stopifnot(length(raw.data[,pos.col.header][raw.data[,rsid.col.header] == snp]) <= 1)
+                if (length(raw.data[,pos.col.header][raw.data[,rsid.col.header] == snp]) >= 1) {
+                    locus.pos.center <- raw.data[,pos.col.header][raw.data[,rsid.col.header] == snp][1]
+                    all.colors[abs(raw.data[,pos.col.header] - locus.pos.center) < locus.width] <- prev.loci.color
                                         #handle label
-                snp.labels <- c(snp.labels, snp.label)
-                snp.label.x.positions <- c(snp.label.x.positions, locus.pos.center)
-                snp.label.y.positions <- c(snp.label.y.positions, max(raw.data[,pval.col.header][abs(raw.data[,pos.col.header] - locus.pos.center) < locus.width]))
-                snp.label.colors <- c(snp.label.colors, prev.loci.color)
+                    snp.labels <- c(snp.labels, snp.label)
+                    snp.label.x.positions <- c(snp.label.x.positions, locus.pos.center)
+                    snp.label.y.positions <- c(snp.label.y.positions, max(raw.data[,pval.col.header][abs(raw.data[,pos.col.header] - locus.pos.center) < locus.width]))
+                    snp.label.colors <- c(snp.label.colors, prev.loci.color)
+                }
             }
         }
-                                        #for each set of previously known loci, overwrite the color as known.loci.color
+                                        #for each set of previously unknown loci, overwrite the color as novel.loci.color
         if (!is.na(filename.novel.hits)) {
             novel.hits <- read.table(filename.novel.hits, header=FALSE)
             novel.hits[,1] <- as.vector(novel.hits[,1], mode="character")
--- shared-source/manhattan_loci_colored_launcher.R
+++ shared-source/manhattan_loci_colored_launcher.R
@@ -2,10 +2,16 @@ cargs <- commandArgs(trailingOnly=TRUE)
 filename.pvals <- cargs[1]
 trait.name <- cargs[2]
 output.filestem <- cargs[3]
+known.signals.file <- NA
 source.dir <- cargs[4]
+
+if (length(cargs) > 4) {
+    known.signals.file <- cargs[4]
+    source.dir <- cargs[5]
+}
 gws.threshold <- 5e-8
 truncate.p <- 1e-50
 
 source(paste(source.dir, "/manhattan_loci_colored.R", sep=""))
 
-manhattan.coded.hits(filename.pvals, trait.name, output.filestem=output.filestem, gws.threshold=gws.threshold, truncate.p=truncate.p)
+manhattan.coded.hits(filename.pvals, trait.name, filename.prev.hits=known.signals.file, output.filestem=output.filestem, gws.threshold=gws.threshold, truncate.p=truncate.p)
