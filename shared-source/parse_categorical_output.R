0b67420b8443c0d8d96491f87e156047886507f3
palmercd <cameron.palmer@nih.gov>
2021-01-07 10:56:18 -0500

    fix: repair and accelerate categorical data processing
    the R script for categorical data test statistic estimation in ldscore regression was slow and
    buggy. it's now less of both of those things

--- shared-source/parse_categorical_output.R
+++ shared-source/parse_categorical_output.R
@@ -18,6 +18,20 @@ effective.stat <- qchisq(input.file[, ncol(input.file) - 1],
   lower.tail = FALSE
 )
 
+n.entries <- (ncol(input.file) - 8) / 2
+min.index <- apply(
+  as.matrix(input.file[, seq(7 + n.entries, 7 + 2 * n.entries - 1)]),
+  1,
+  which.min
+)
+min.index <- min.index + 6
+
+target.effect <- input.file[, 7]
+for (i in seq(8, n.entries + 6)) {
+  target.effect[min.index == i] <- input.file[min.index == i, i]
+}
+effective.stat <- sqrt(effective.stat) * sign(target.effect)
+
 output.data <- data.frame(
   SNP = paste(input.file[, 1], input.file[, 2], sep = ":"),
   Tested_Allele = input.file[, 4],
@@ -28,8 +42,8 @@ output.data <- data.frame(
   N = input.file[, ncol(input.file)]
 )
 
-write.table(effective.stat,
-  output.data,
+write.table(output.data,
+  output.file,
   row.names = FALSE,
   col.names = TRUE,
   quote = FALSE,
